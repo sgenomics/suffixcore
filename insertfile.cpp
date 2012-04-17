7007e99013bc4ceddf49bf9006265932a31b528f
Nava Whiteford <new@sgenomics.org>
2012-04-17 12:00:54 +0100

    Added license and license_headers

--- insertfile.cpp
+++ insertfile.cpp
@@ -1,3 +1,18 @@
+/* -
+ * Copyright (c) 2012 Nava Whiteford <new@sgenomics.org>
+ * suffixcore - core suffixtree algorithms
+ *
+ * A license to use this software is granted to users given access to the git repository at: https://github.com/new299/suffixcore
+ * A complete copy of this license is located in the license file of this package.
+ *
+ * In Summary this software:
+ *
+ * Can be used for creating unlimited applications.
+ * Can be distributed in binary or object form only.
+ * Commercial use is allowed.
+ * Can modify source-code but cannot distribute modifications (derivative works).
+ */
+
 #include <iostream>
 #include <string>
 #include <vector>
